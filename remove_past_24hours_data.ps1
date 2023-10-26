a6cfe20981f6fa76f1e2adadd18badf1c5854770
arifams <arif.amsterdam@gmail.com>
2023-10-26 13:29:00 +0200

    Update remove_past_24hours_data.ps1

--- remove_past_24hours_data.ps1
+++ remove_past_24hours_data.ps1
@@ -3,3 +3,14 @@ get-childitem -Path C:\path\where\do\you\need\to\remove\data\ –recurse |
     where-object {-not $_.PSIsContainer} |
     Foreach-Object { $_.FullName } | 
     Remove-Item
+
+
+# remove past 24 hours data in multiple folders #
+
+$removedFolder = 'C:\Users\ahoy\Downloads\','C:\Users\ahoy\Pictures\' ,'C:\Users\ahoy\Documents\'
+
+get-childitem -Path $removedFolder -Recurse | 
+    where-object {$_.lastwritetime -gt (get-date).addDays(-1)} | 
+    where-object {-not $_.PSIsContainer} |
+    Foreach-Object { $_.FullName } | 
+    Remove-Item
